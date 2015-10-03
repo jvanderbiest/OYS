cb13a84fdd5956b560d09073e37e6a901d20837c
jvanderbiest <jvanderbiest@gmail.com>
2015-10-03 15:58:45 +0200

    fixed running tests

--- gulp/tasks/protractor.js
+++ gulp/tasks/protractor.js
@@ -11,7 +11,8 @@ gulp.task('webdriver', webdriver);
 
 gulp.task('protractor', ['webdriver-update', 'webdriver', 'server'], function(cb) {
 
-  gulp.src('test/e2e/**/*.js').pipe(protractor({
+  /*http://stackoverflow.com/questions/32844090/gulp-protractor-keep-saying-spec-patterns-did-not-match-any-files*/
+  gulp.src([]).pipe(protractor({
       configFile: config.test.protractor
   })).on('error', function(err) {
     // Make sure failed tests cause gulp to exit non-zero
